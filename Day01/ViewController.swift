f7006f071bca348b6f5b93dbd6505633b23ef3a7
wmfadmin <1450931080@qq.com>
2016-05-20 14:49:58 +0800

    init
    init

---
+++ Day01/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  Day01
+//
+//  Created by 800best'pro used by wmf on 16/4/14.
+//  Copyright © 2016年 800best. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var display: UILabel!
+    var  userIsInTheMiddleOfTypingANumber :Bool =  false
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!;
+        NSLog("digit =%@", digit);
+        if userIsInTheMiddleOfTypingANumber{
+            display.text = display.text! + digit;
+        }else{
+            userIsInTheMiddleOfTypingANumber = true;
+            display.text = digit;
+        }
+        
+    }
+    
+    var optionStack = Array <Double>();
+   
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false;
+        optionStack.append(displayValue)
+    }
+    
+
+    @IBAction func operate(sender: UIButton) {
+        
+        _ = sender.currentTitle!;
+        if userIsInTheMiddleOfTypingANumber {
+            enter();
+        }
+//        switch operation{
+//            case "x":
+//              if optionStack.count>=2{
+//                    
+//            }
+//            case "x":
+//              if optionStack.count>=2{
+//                
+//            }
+//            case "x":
+//              if optionStack.count>=2{
+//                
+//            }
+//            case "x":
+//              if optionStack.count>=2{
+//                
+//            }
+//            
+//        default:
+//
+//        }
+        
+    }
+    var displayValue:Double{
+        get{
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue;
+        }
+        set{
+            
+        }
+}
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        
+        
+    }
+
+
+}
+
